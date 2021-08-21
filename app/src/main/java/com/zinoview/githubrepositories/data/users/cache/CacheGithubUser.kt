ee036b6495120d7fbcd1aae06c402f35bb9be8d7
KostyaGig <k.gig@list.ru>
2021-08-21 21:55:10 +0300

    other file

---
+++ app/src/main/java/com/zinoview/githubrepositories/data/users/cache/CacheGithubUser.kt
@@ -0,0 +1,30 @@
+package com.zinoview.githubrepositories.data.users.cache
+
+import android.util.Log
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import com.zinoview.githubrepositories.core.Abstract
+import com.zinoview.githubrepositories.data.users.DataGithubUser
+
+
+/**
+ * @author Zinoview on 19.08.2021
+ * k.gig@list.ru
+ */
+
+@Entity(tableName = "github_users_table")
+data class CacheGithubUser(
+    @PrimaryKey
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "bio")
+    val bio: String,
+    @ColumnInfo(name = "profileImageUrl")
+    val profileImageUrl: String
+) : Abstract.Object.Data {
+
+    override fun map(mapper: Abstract.UserMapper<DataGithubUser>): DataGithubUser
+        = mapper.map(name,bio,profileImageUrl)
+}
